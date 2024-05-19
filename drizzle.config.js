d29a1d20cce7ba3e2aa4441d571674a8b67fdd78
ibrahimberkozkan <ibrahimberkozkan34@gmail.com>
2024-05-19 03:21:37 +0300

    secret keys deleted (oops)

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,6 +2,6 @@ export default {
     schema: "./utils/schema.jsx",
     dialect: 'postgresql',
     dbCredentials: {
-        url: "postgresql://moneydb_owner:ySLAKY1DI8wB@ep-dry-moon-a5wpehsh.us-east-2.aws.neon.tech/Expenses-Tracker?sslmode=require"
+        url: "url"
     }
 };
--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -2,7 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
 
-const sql = neon("postgresql://moneydb_owner:ySLAKY1DI8wB@ep-dry-moon-a5wpehsh.us-east-2.aws.neon.tech/Expenses-Tracker?sslmode=require");
+const sql = neon("url");
 const db = drizzle(sql, {schema});
 
 export { db };
